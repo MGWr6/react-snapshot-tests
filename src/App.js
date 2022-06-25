973b51037a802e5083fa39d12e8a0d1c97a956b9
Mason Wagoner <mgwagoner@gmail.com>
2022-06-24 22:13:58 -0400

    first commit initial testing complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, natus!</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
