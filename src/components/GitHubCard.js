ca0bb2c5c8dce885ce425973343f9955df431f6f
Mason Wagoner <mgwagoner@gmail.com>
2022-06-30 19:31:44 -0400

    added github link

--- src/components/GitHubCard.js
+++ src/components/GitHubCard.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import SamplePic from '../SamplePic.png'
+import React from "react";
+import SamplePic from "../SamplePic.png";  
 
 function GitHubCard() {
   return (
-  <div data-testId="gitHubCard-1">
-  <div className="card">React Snapshot tests - GitHub Mockup</div>
-  <img 
-    styles={{ minWidth: 300, minHeight: 300 }}
-    src={SamplePic} alt="Card cap" 
-  />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non, nesciunt.</p>
-  </div>
-  </div>
-  )
+    <div data-testId="gitHubCard-1">
+      <div className="card">React Snapshot tests - GitHub Mockup</div>
+      <img
+        styles={{ minWidth: 300, minHeight: 300 }}
+        src={SamplePic}
+        alt="Card cap"
+      />
+      <div className="card-body">
+        <h3><a href="https://github.com/MGWr6">My GitHub</a></h3>
+        <p className="card-text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non,
+          nesciunt. 
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default GitHubCard
+export default GitHubCard;
